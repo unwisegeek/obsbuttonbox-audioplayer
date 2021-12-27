a3b7c0709beefe75369907992c0071c1f34e5da0
John Madon <john@unwisegeek.net>
2021-12-27 18:49:10 -0500

    Clarified sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,5 +1,6 @@
 # MQTT Configuration
 MQTT_HOST = None # Example: 127.0.0.1:3000 or localhost:3000
+#MQTT_HOST = ""
 MQTT_PORT = 0  # Do not use '' - Example: 1883
 MQTT_AUTH = None # Comment this line and uncomment the line below to use authentication with MQTT
 #MQTT_AUTH = {'username':'','password':''} # Authentication for MQTT. Uncomment if required.
